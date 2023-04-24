05a195998585a587044e6c523bb3d720754b3de0
jobemcamera <jobecamera@gmail.com>
2023-04-24 18:56:31 -0300

    conexão e resposta da API boostablebosses estabelecida

--- src/pages/Bosses/index.jsx
+++ src/pages/Bosses/index.jsx
@@ -1,11 +1,30 @@
 import Form from 'components/Form'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Bosses() {
+
+    const [bosses, setBosses] = useState([])
+
+    useEffect(() => {
+        const boostableBosses = async () => {
+            const response = await fetch('https://api.tibiadata.com/v3/boostablebosses');
+            const jsonData = await response.json();
+            setBosses(jsonData.boostable_bosses.boostable_boss_list);
+        };
+
+        boostableBosses();
+
+    }, [])
+
     return (
         <>
             <Form name="Boss" />
-            <h1></h1>
+
+            <div>
+                <ul>
+                    {bosses.map(boss => <li key={boss.name}>{boss.name}</li>)}
+                </ul>
+            </div>
         </>
     )
 }
