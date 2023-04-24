af1acc95d9cb9ef622d8fcb18aba20fafe3f2b25
jobemcamera <jobecamera@gmail.com>
2023-04-24 18:17:59 -0300

    arredondamento das bordas da DefaultPage e Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import styles from './Home.module.scss'
 
 export default function Home() {
     return (
-        <main className={styles.home}>
+        <div className={styles.home}>
             <h1>Home</h1>
 
             <article>
@@ -11,6 +11,6 @@ export default function Home() {
                 <p>My name is Jobe Camera and this is a React App page for studying. The main goal of this app is to improve my skills in developing with React and APIs. The case study for this project is Tibia Data because I love Tibia, of course.</p>
                 <p>In this app, you can find the Tibia content such as creatures, bosses, characters, and worlds.</p>
             </article>
-        </main>
+        </div>
     )
 }
