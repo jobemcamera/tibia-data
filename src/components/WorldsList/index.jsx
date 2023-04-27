b48bded483e8f6dae29a1e2143baf1fc8e65d41d
jobemcamera <jobecamera@gmail.com>
2023-04-27 19:49:32 -0300

    componente WorldsList

--- src/components/WorldsList/index.jsx
+++ src/components/WorldsList/index.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import styles from './WorldsList.module.scss'
 
 export default function WorldsList({ worlds }) {
     return (
         <>
-            {worlds.map(world => <p key={world.name}>{world.name}</p>)}
+            <section>
+                <table className={styles}>
+                    <thead>
+                        <tr>
+                            <th>World</th>
+                            <th>Online</th>
+                            <th>Location</th>
+                            <th>PvP Type</th>
+                            <th>Transfer Type</th>
+                            <th>BattleEye</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {worlds.map(world => (
+                            <tr key={world.name}>
+                                <td>{world.name}</td>
+                                <td>{world.players_online}</td>
+                                <td>{world.location}</td>
+                                <td>{world.pvp_type}</td>
+                                <td>{world.transfer_type}</td>
+                                <td>{world.battleye_protected}</td>
+                            </tr>)
+                        )}
+                    </tbody>
+                </table>
+            </section>
         </>
     )
 }
