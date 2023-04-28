7f39833666d44a796b44632f5ee0600bf2a389d6
jobemcamera <jobecamera@gmail.com>
2023-04-27 21:41:43 -0300

    tabela do componente WorldsList com bandeira dos países e ícone do BattlEye

--- src/components/WorldsList/index.jsx
+++ src/components/WorldsList/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import styles from './WorldsList.module.scss'
+// import greenBE from 'assets/icon_battleyeinitial.gif'
+// import yellowBE from 'assets/icon_battleye.gif'
 
 export default function WorldsList({ worlds }) {
+
     return (
         <>
             <section>
@@ -21,10 +24,38 @@ export default function WorldsList({ worlds }) {
                             <tr key={world.name}>
                                 <td>{world.name}</td>
                                 <td>{world.players_online}</td>
-                                <td>{world.location}</td>
+                                <td>{
+                                    world.location === 'Europe' ? (
+                                        <div className={styles.flags}>
+                                            <p>{world.location}</p>
+                                            <img src="https://www.bandeirasnacionais.com/data/flags/w580/gb.webp" alt="The flag of The United Kingdom" />
+                                        </div>
+                                    ) : world.location === 'South America' ? (
+                                        <div className={styles.flags}>
+                                            <p>{world.location}</p>
+                                            <img src="https://www.bandeirasnacionais.com/data/flags/w580/br.webp" alt="The flag of Brazil" />
+                                        </div>
+                                    ) : world.location === 'North America' ? (
+                                        <div className={styles.flags}>
+                                            <p>{world.location}</p>
+                                            <img src="https://www.bandeirasnacionais.com/data/flags/w580/us.webp" alt='The flag of The United States of America' />
+                                        </div>
+                                    ) : null
+                                }
+                                </td>
                                 <td>{world.pvp_type}</td>
                                 <td>{world.transfer_type}</td>
-                                <td>{world.battleye_protected}</td>
+                                <td>
+                                    {
+                                        world.battleye_date == "release" ? (
+                                            <img src={'https://static.tibia.com/images/global/content/icon_battleyeinitial.gif'} alt='Yellow BattlEye item' />
+                                        ) : world.battleye_date == "" ? (
+                                                <p></p>
+                                        ): (
+                                            <img src = { 'https://static.tibia.com/images/global/content/icon_battleye.gif' } alt = 'Green BattlEye item' />
+                                        )
+                                    }
+                                </td>
                             </tr>)
                         )}
                     </tbody>
--- src/pages/Worlds/index.jsx
+++ src/pages/Worlds/index.jsx
@@ -1,6 +1,6 @@
-import Form from 'components/Form'
+import Form from 'components/Form';
 import WorldsList from 'components/WorldsList';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 export default function Worlds() {
 
@@ -14,8 +14,36 @@ export default function Worlds() {
         };
 
         worlds();
-
+        
     }, []);
+
+    // const protectedBattlEye = () => {
+    //     return worlds.map( (local, index) => {
+    //         const battlEyeDate = new Date(local.battleye_date);
+    //         const year = battlEyeDate.getFullYear();
+    //         const month = battlEyeDate.getMonth() + 1;
+    //         const day = battlEyeDate.getDate();
+
+    //         if (battlEyeDate != "Invalid Date") {
+    //             console.log(`${year}-${month}-${day}`)
+    //         }
+    //     })
+    // }
+
+    // const protectedBattlEye = () => {
+    //     return worlds.map( local => {
+    //         if (local.battleye_date != "release" && local.battleye_date != "") {
+    //             <div key={local.battleye_date}>
+    //                 <img src={greenBE} alt="Green BattlEye" />
+    //             </div>
+    //         } else {
+    //             <div key={local.battleye_date}>
+    //                 <img src={yellowBE} alt="Yellow BattlEye" />
+    //             </div>
+    //         }
+    //     })
+    // }
+    // protectedBattlEye();
 
     return (
         <>
