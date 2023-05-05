4a7e4d4e9d95c94e56619075b2e45c5861c6bf19
jobemcamera <jobecamera@gmail.com>
2023-05-05 09:05:15 -0300

    criação dos filtros em WorldsList: Location, PvPType e TransferType

--- src/components/WorldsList/index.jsx
+++ src/components/WorldsList/index.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './WorldsList.module.scss'
 import { Link } from 'react-router-dom'
 
 export default function WorldsList({ worlds }) {
+
+    const [filters, setFilters] = useState({
+        location: '',
+        pvp_type: '',
+        transfer_type: '',
+    });
+
+    const handleFilterChange = (event) => {
+        const { name, value } = event.target;
+        setFilters((prevFilters) => ({
+            ...prevFilters,
+            [name]: value,
+        }));
+    };
+
+    const filteredWorlds = worlds.filter((world) => {
+        const isLocationMatched = filters.location === '' || filters.location === world.location;
+        const isPvpTypeMatched = filters.pvp_type === '' || filters.pvp_type === world.pvp_type;
+        const isTransferTypeMatched = filters.transfer_type === '' || filters.transfer_type === world.transfer_type;
+
+        return isLocationMatched && isPvpTypeMatched && isTransferTypeMatched
+    });
+
 
     return (
         <>
@@ -12,14 +35,46 @@ export default function WorldsList({ worlds }) {
                         <tr>
                             <th>World</th>
                             <th>Online</th>
-                            <th>Location</th>
-                            <th>PvP Type</th>
-                            <th>Transfer Type</th>
+                            <th>
+                                <div>
+                                    <span>Location </span>
+                                    <select name="location" value={filters.location} onChange={handleFilterChange}>
+                                        <option value="">All</option>
+                                        <option value="Europe">Europe</option>
+                                        <option value="South America">South America</option>
+                                        <option value="North America">North America</option>
+                                    </select>
+                                </div>
+                            </th>
+                            <th>
+                                <div>
+                                    <span>PvP Type </span>
+                                    <select name="pvp_type" value={filters.pvp_type} onChange={handleFilterChange}>
+                                        <option value="">All</option>
+                                        <option value="Open PvP">Open PvP</option>
+                                        <option value="Optional PvP">Optional PvP</option>
+                                        <option value="Hardcore PvP">Hardcore PvP</option>
+                                        <option value="Retro Open PvP">Retro Open PvP</option>
+                                        <option value="Retro Hardcore PvP">Retro Hardcore PvP</option>
+                                    </select>
+                                </div>
+                            </th>
+                            <th>
+                                <div>
+                                    <span>Transfer Type </span>
+                                    <select name="transfer_type" value={filters.transfer_type} onChange={handleFilterChange}>
+                                        <option value="">All</option>
+                                        <option value="regular">Regular</option>
+                                        <option value="blocked">Blocked</option>
+                                        <option value="locked">Locked</option>
+                                    </select>
+                                </div>
+                            </th>
                             <th>BattleEye</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {worlds.map(world => (
+                        {filteredWorlds.length > 0 ? filteredWorlds.map(world => (
                             <tr key={world.name}>
                                 <td>
                                     <Link to={world.name}>
@@ -59,8 +114,13 @@ export default function WorldsList({ worlds }) {
                                         )
                                     }
                                 </td>
-                            </tr>)
-                        )}
+                            </tr>
+                        )) : (
+                            <tr>
+                                <td colSpan={6} className={styles.not__found}>Not found</td>
+                            </tr>
+                            )
+                        }
                     </tbody>
                 </table>
             </section>
