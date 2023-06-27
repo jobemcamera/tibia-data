3ae4ce8fe6f535043259ad144426fd385ecddc16
jobemcamera <jobecamera@gmail.com>
2023-06-27 09:07:09 -0300

    configurando validade do Form quando input e submetido

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -3,25 +3,29 @@ import React, { useRef } from "react";
 import styles from "./Form.module.scss";
 import Button from "components/Button";
 
-export default function Form({ name, onSearchCharacter }) {
+export default function Form({ name, validField, onSearchCharacter }) {
   const characterInputRef = useRef();
 
-	const handleSubmit = () => {
-		const enteredCharacter = characterInputRef.current.value;
+  const handleSubmit = () => {
+    const enteredCharacter = characterInputRef.current.value;
     onSearchCharacter(enteredCharacter);
-	}
+  };
 
   function sendForm(event) {
     event.preventDefault();
   }
+
+  const inputStyle = `${styles.field__container__input} ${
+    validField ? styles.invalid : ""
+  }`;
 
   return (
     <section className={styles.form__container}>
       <MainTitle title={name} />
       <form onSubmit={sendForm}>
         <div className={styles.field__container}>
-          <label>{name}</label>
-          <input type="text" ref={characterInputRef} />
+          <label className={styles.field__container__label}>{name}</label>
+          <input type="text" ref={characterInputRef} className={inputStyle} />
           <Button title="Submit" action={handleSubmit} />
         </div>
       </form>
