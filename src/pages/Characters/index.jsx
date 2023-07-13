2b7f317f0bcf3cfdef0ac1bac3fdefb198a9c332
Jobe Moraes <jobecamera@gmail.com>
2023-07-13 08:59:46 -0300

    configurando o fetch api do character

--- src/pages/Characters/index.jsx
+++ src/pages/Characters/index.jsx
@@ -1,24 +1,33 @@
-import Form from 'components/Form'
-import React, { useState } from 'react'
+import Character from 'components/Character';
+import Form from 'components/Form';
+import React, { useState } from 'react';
 
 export default function Characters() {
+  const [character, setCharacter] = useState('');
 
-    const [characaters, setCharcters] = useState('');
-
-    const searchCharacterHandler = (enteredCharacter) => {
-
-        if (enteredCharacter.trim().length === 0) {
-            console.log('vazio')
-            return;
-        }
-
-        setCharcters(enteredCharacter);
-        console.log(enteredCharacter)
+  const searchCharacterHandler = (enteredCharacter) => {
+    if (enteredCharacter.trim().length === 0) {
+      console.log('vazio');
+      return;
     }
 
-    return (
-        <>
-            <Form name="Search Character" onSearchCharacter={searchCharacterHandler}/>
-        </>
-    )
+    const formattedName = enteredCharacter.replace(/ /g, '%20');
+
+    fetch(`https://api.tibiadata.com/v3/character/${formattedName}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacter(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <>
+      <Form name="Search Character" onSearchCharacter={searchCharacterHandler} />
+      <Character character={character} />
+    </>
+  );
 }
