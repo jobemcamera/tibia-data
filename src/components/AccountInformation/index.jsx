e0545169ab90f2c5e0acf94820ed19890a31ae40
Jobe Moraes <jobecamera@gmail.com>
2023-07-22 20:52:09 -0300

    refatorando AccountInformation

--- src/components/AccountInformation/index.jsx
+++ src/components/AccountInformation/index.jsx
@@ -1,14 +1,18 @@
+import React from "react";
 import MainTitle from "components/MainTitle";
 import { formatDate } from "components/SharedFunctions";
 import styles from './AccountInformation.module.scss';
 
 export default function AccountInformation({ character }) {
   const renderCharacterInfo = () => {
+    if (Object.keys(character).length == 0) {
+      return;
+    }
+
     const characterInfo = [
       { label: 'Loyalty Title', value: character.loyalty_title },
-      { label: 'Created', value: formatDate(character.created, 'MMM DD YYYY, HH:mm:ss z'), },
-    ];
-
+      { label: 'Created', value: formatDate(character.created, 'MMM DD YYYY, HH:mm:ss z') },
+    ]
 
     return characterInfo.map((info) => {
       if (!info || info.value === null) {
@@ -22,16 +26,20 @@ export default function AccountInformation({ character }) {
         </tr>
       );
     });
-  };
-  return (
-    <section className={styles.container}>
-      <MainTitle title="Account Information" />
-      <table>
-        <tbody>
-          {character && renderCharacterInfo()}
-        </tbody>
-      </table>
-    </section>
+  }
 
-  )
+  return (
+    <>
+      {renderCharacterInfo() &&
+        <section className={styles.container}>
+          <MainTitle title="Account Information" />
+          <table>
+            <tbody>
+              {renderCharacterInfo()}
+            </tbody>
+          </table>
+        </section>
+      }
+    </>
+  );
 }
