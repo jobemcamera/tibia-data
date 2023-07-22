5afa006d3d48c74f6351c39e94e404efb72de269
Jobe Moraes <jobecamera@gmail.com>
2023-07-22 20:54:44 -0300

    chamando o componente OtherCharacters

--- src/components/Character/index.jsx
+++ src/components/Character/index.jsx
@@ -1,18 +1,21 @@
 import AccountInformation from "components/AccountInformation";
 import CharacterAchievements from "components/CharacterAchievements";
 import CharacterInformation from "components/CharacterInformation";
+import OtherCharacters from "components/OtherCharacters";
 import React from "react";
 
 export default function Character({ character }) {
 
   const characterInformation = character.character;
   const characterAchievements = character.achievements;
-  const accountInformation = character.account_information
+  const accountInformation = character.account_information;
+  const otherCharacters = character.other_characters;
   return (
     <>
       {characterInformation && (<CharacterInformation character={characterInformation} />)}
       <CharacterAchievements character={characterAchievements} />
       <AccountInformation character={accountInformation} />
+      <OtherCharacters character={otherCharacters} />
     </>
   )
 }
