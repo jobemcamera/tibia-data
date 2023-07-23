e5fff4f8cf50ab9df435026b62225786983cd269
Jobe Moraes <jobecamera@gmail.com>
2023-07-23 09:58:28 -0300

    refatorando formatDate

--- src/components/SharedFunctions/index.jsx
+++ src/components/SharedFunctions/index.jsx
@@ -2,16 +2,28 @@ import React from 'react'
 import Moment from 'react-moment'
 import 'moment-timezone';
 
-export function openNewTab(link){
-    return () => {
-        window.open(link, '_blank')
-    }
+export function openNewTab(link) {
+	return () => {
+		window.open(link, '_blank')
+	}
 }
 
 export function formatDate(date, style) {
+  const inputDateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/;
+
+  // Check if the input is in "YYYY-MM-DDTHH:mm:ssZ" format
+  if (inputDateFormat.test(date)) {
     return (
-        <Moment format={style} tz="Europe/Paris">
-            {date}
-        </Moment>
-    )
+      <Moment format={style} tz="Europe/Paris">
+        {date}
+      </Moment>
+    );
+  } else {
+    // If the input is not in the above format, assume it's in "YYYY-MM-DD" format
+    return (
+      <Moment format={style}>
+        {date}
+      </Moment>
+    );
+  }
 }
