e57165184721154d1e7722787038cd78b47b8c9b
Jobe Moraes <jobecamera@gmail.com>
2023-07-22 23:17:31 -0300

    atribuindo loading ao buscar character

--- src/pages/Characters/index.jsx
+++ src/pages/Characters/index.jsx
@@ -1,13 +1,17 @@
 import Character from 'components/Character';
 import Form from 'components/Form';
 import React, { useState } from 'react';
+import Loading from 'components/Loading';
 
 export default function Characters() {
   const [character, setCharacter] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const searchCharacterHandler = async (enteredCharacter) => {
+    setLoading(true);
+
     if (enteredCharacter.trim().length === 0) {
-      console.log('vazio');
+      setLoading(false);
       return;
     }
 
@@ -16,16 +20,24 @@ export default function Characters() {
       const response = await fetch(`https://api.tibiadata.com/v3/character/${formattedName}`);
       const data = await response.json();
       const characterData = data.characters;
-      console.log(characterData);
-      setCharacter(characterData);
+      if (characterData.character && characterData.character.name.trim() !== '') {
+        setCharacter(characterData);
+      } else {
+        return;
+      }
+      setLoading(false);
     } catch (error) {
       console.log(error);
+      setLoading(false);
     }
   };
 
   return (
     <>
-      {character && Object.keys(character).length > 0 && <Character character={character} />}
+      {character && Object.keys(character).length > 0 && !loading && (
+        <Character character={character} />
+      )} 
+      {loading && <Loading />} 
       <Form name="Search Character" onSearchCharacter={searchCharacterHandler} />
     </>
   );
