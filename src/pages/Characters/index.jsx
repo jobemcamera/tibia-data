3f239e1bd2cb784e54376a9527bda3dba5d07171
Jobe Moraes <jobecamera@gmail.com>
2023-07-22 23:42:37 -0300

    chamando o componente CharacterNotFound, criando state para renderiza-lo

--- src/pages/Characters/index.jsx
+++ src/pages/Characters/index.jsx
@@ -2,13 +2,17 @@ import Character from 'components/Character';
 import Form from 'components/Form';
 import React, { useState } from 'react';
 import Loading from 'components/Loading';
+import CharacterNotFound from 'components/CharacterNotFound';
 
 export default function Characters() {
   const [character, setCharacter] = useState(null);
+  const [characterName, setCharacterName] = useState('');
   const [loading, setLoading] = useState(false);
+  const [characterNotFound, setCharacterNotFound] = useState(false);
 
   const searchCharacterHandler = async (enteredCharacter) => {
     setLoading(true);
+    setCharacterNotFound(false);
 
     if (enteredCharacter.trim().length === 0) {
       setLoading(false);
@@ -23,11 +27,15 @@ export default function Characters() {
       if (characterData.character && characterData.character.name.trim() !== '') {
         setCharacter(characterData);
       } else {
-        return;
+        setCharacterName(enteredCharacter);
+        setCharacterNotFound(true);
+        setCharacter(null);
       }
       setLoading(false);
     } catch (error) {
-      console.log(error);
+      setCharacterName(enteredCharacter);
+      setCharacterNotFound(true);
+      setCharacter(null);
       setLoading(false);
     }
   };
@@ -36,7 +44,8 @@ export default function Characters() {
     <>
       {character && Object.keys(character).length > 0 && !loading && (
         <Character character={character} />
-      )} 
+      )}
+      {characterNotFound && !loading && <CharacterNotFound character={characterName} />}
       {loading && <Loading />} 
       <Form name="Search Character" onSearchCharacter={searchCharacterHandler} />
     </>
