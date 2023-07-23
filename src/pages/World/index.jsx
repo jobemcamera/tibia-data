be584a68fdd5a8fe17678a799281cd4c68066fdf
Jobe Moraes <jobecamera@gmail.com>
2023-07-23 09:59:20 -0300

    refatorando World (info)

--- src/pages/World/index.jsx
+++ src/pages/World/index.jsx
@@ -6,94 +6,78 @@ import Button from 'components/Button';
 import { formatDate } from 'components/SharedFunctions';
 
 export default function World() {
-    const { world } = useParams();
+  const { world } = useParams();
+  const [worldInfo, setWorldInfo] = useState({});
 
-    const [worldInfo, setWorldInfo] = useState([]);
+  useEffect(() => {
+    const fetchWorldInfo = async () => {
+      try {
+        const response = await fetch(`https://api.tibiadata.com/v3/world/${world}`);
+        const jsonData = await response.json();
+        setWorldInfo(jsonData.worlds.world);
+        console.log(jsonData.worlds.world)
+      } catch (error) {
+        console.error("Error fetching world info:", error);
+      }
+    };
 
-    useEffect(() => {
-        const worldsInfo = async () => {
-            const response = await fetch(`https://api.tibiadata.com/v3/world/${world}`);
-            const jsonData = await response.json();
-            setWorldInfo(jsonData.worlds.world);
-        };
+    fetchWorldInfo();
+  }, [world]);
 
-        worldsInfo();
+  const back = useNavigate();
 
-    }, [world]);
+  function backPage() {
+    return back(-1);
+  }
+  // "record_date": "2020-05-01T15:58:30Z",
+  // "last_login": "2020-08-31T13:47:00Z",
+  const tableData = [
+    { label: "Status", value: worldInfo.status },
+    { label: "Players Online", value: worldInfo.players_online },
+    {
+      label: "Online Record",
+      value: worldInfo.record_players
+        ? `${worldInfo.record_players} players on ${formatDate(worldInfo.record_date, "MMMM D YYYY, HH:mm:ss z")}` 
+        : null,
+    },
+    {
+      label: "Creation Date",
+      value: worldInfo.creation_date ? formatDate(worldInfo.creation_date, "MMMM YYYY") : null,
+    },
+    { label: "Location", value: worldInfo.location },
+    { label: "PvP Type", value: worldInfo.pvp_type },
+    {
+      label: "World Quest Titles",
+      value: worldInfo.world_quest_titles && worldInfo.world_quest_titles.length > 0
+        ? worldInfo.world_quest_titles.join(', ')
+        : "N/A",
+    },
+    {
+      label: "BattlEye Status",
+      value: worldInfo.battleye_protected === true
+        ? worldInfo.battleye_date !== "release"
+          ? `Protected by BattlEye since ${formatDate(worldInfo.battleye_date, "MMMM DD, YYYY")}`
+          : "Protected by BattlEye since its release."
+        : "Not protected by BattlEye.",
+    },
+    { label: "Game World Type", value: worldInfo.game_world_type },
+  ];
 
-    const back = useNavigate();
-
-    function backPage() {
-        return back(-1);
-    }
-
-    return (
-        <section>
-            <MainTitle title={world} >
-                <Button title="Back" action={backPage} />
-            </MainTitle>
-
-            <div>
-                <table className={styles.world__table}>
-                    <tbody className={styles.world__table__tbody}>
-
-                        <tr className={styles.world__table__tbody__tr}>
-                            <td className={styles.world__table__tbody__tr__td}>Status:</td>
-                            <td className={styles.world__table__tbody__tr__td}>{worldInfo.status}</td>
-                        </tr>
-                        <tr className={styles.world__table__tbody__tr}>
-                            <td className={styles.world__table__tbody__tr__td}>Players Online:</td>
-                            <td className={styles.world__table__tbody__tr__td}>{worldInfo.players_online}</td>
-                        </tr>
-                        <tr className={styles.world__table__tbody__tr}>
-                            <td className={styles.world__table__tbody__tr__td}>Online Record:</td>
-                            <td className={styles.world__table__tbody__tr__td}>
-                                {`${worldInfo.record_players} players on `}
-                                {formatDate(worldInfo.record_date, "MMMM D YYYY, HH:mm:ss z")}
-                            </td>
-                        </tr>
-                        <tr className={styles.world__table__tbody__tr}>
-                            <td className={styles.world__table__tbody__tr__td}>Creation Date:</td>
-                            <td className={styles.world__table__tbody__tr__td}>
-                                {formatDate(worldInfo.creation_date, "MMMM YYYY")}
-                            </td>
-                        </tr>
-                        <tr className={styles.world__table__tbody__tr}>
-                            <td className={styles.world__table__tbody__tr__td}>Location:</td>
-                            <td className={styles.world__table__tbody__tr__td}>{worldInfo.location}</td>
-                        </tr>
-                        <tr className={styles.world__table__tbody__tr}>
-                            <td className={styles.world__table__tbody__tr__td}>PvP Type:</td>
-                            <td className={styles.world__table__tbody__tr__td}>{worldInfo.pvp_type}</td>
-                        </tr>
-                        <tr className={styles.world__table__tbody__tr}>
-                            <td className={styles.world__table__tbody__tr__td}>World Quest Titles:</td>
-                            <td className={styles.world__table__tbody__tr__td}>
-                                {worldInfo && worldInfo.world_quest_titles && worldInfo.world_quest_titles.length > 0 ? worldInfo.world_quest_titles.join(', ') : "N/A"}
-                            </td>
-                        </tr>
-                        <tr className={styles.world__table__tbody__tr}>
-                            <td className={styles.world__table__tbody__tr__td}>BattlEye Status:</td>
-                            <td className={styles.world__table__tbody__tr__td}>{
-                                worldInfo.battleye_protected === true && worldInfo.battleye_date !== "release" ?
-                                    (<>
-                                        Protected by BattlEye since {" "}
-                                        {formatDate(worldInfo.battleye_date, "MMMM DD, YYYY")}
-                                    </>)
-                                    : worldInfo.battleye_protected === true ?
-                                        ("Protected by BattlEye since its release.")
-                                        : ("Not protected by BattlEye.")
-                            }
-                            </td>
-                        </tr>
-                        <tr className={styles.world__table__tbody__tr}>
-                            <td className={styles.world__table__tbody__tr__td}>Game World Type:</td>
-                            <td className={styles.world__table__tbody__tr__td}>{worldInfo.game_world_type}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-
-        </section>
-    );
+  return (
+    <section className={styles.container}>
+      <MainTitle title={world}>
+        <Button title="Back" action={backPage} />
+      </MainTitle>
+        <table>
+          <tbody>
+            {tableData.map((item, index) => (
+              <tr key={index}>
+                <td>{item.label}:</td>
+                <td>{item.value || "N/A"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+    </section>
+  );
 }
