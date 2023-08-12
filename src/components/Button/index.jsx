99d19f889660965b2798458146faa96cdc9d3bb6
Jobe Moraes <jobecamera@gmail.com>
2023-08-12 18:58:42 -0300

    refatorando as classes de estilo de Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import styles from "./Button.module.scss";
 
 export default function Button({ title, action }) {
-    return (
-        <div className={styles.button__container}>
-            <button onClick={action}>{title}</button>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <button onClick={action}>{title}</button>
+    </div>
+  )
 }
