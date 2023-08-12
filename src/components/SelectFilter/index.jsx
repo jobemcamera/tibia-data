6f082fbfbc089fc34fc0b1b219bc5b4d2f2d329e
Jobe Moraes <jobecamera@gmail.com>
2023-08-12 20:26:42 -0300

    refatorando estilos de SelectFilter

--- src/components/SelectFilter/index.jsx
+++ src/components/SelectFilter/index.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 import styles from './SelectFilter.module.scss'
 
 export default function SelectFilter({ filters, handleFilterChange, options, info }) {
-    return (
-        <div>
-            <select className={styles.select__filter} name={info} value={filters.info} onChange={handleFilterChange}>
-                {options.map(option => (
-                    <option value={option} className={styles.select__filter__option} key={option}>
-                        {option === "" ? "All" : option === "date" ? "Yellow" : option === "release" ? "Green" : option}
-                    </option>
-                ))}
-            </select>
-        </div>
-    )
+  return (
+    <select className={styles.filter} name={info} value={filters.info} onChange={handleFilterChange}>
+      {options.map(option => (
+        <option value={option} key={option}>
+          {option === "" ? "All" : option}
+        </option>
+      ))}
+    </select>
+  )
 }
