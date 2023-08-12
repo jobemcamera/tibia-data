fae962bfbf18ce03bbb8787ac3e99cc67c03902e
Jobe Moraes <jobecamera@gmail.com>
2023-08-12 20:35:19 -0300

    refatorndo estilos e nome do componente Loading

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import styles from './Loading.module.scss'
 import loading from '../../assets/svg/loading-svgrepo-com.svg'
 
-export default function Loader() {
-    return (
-        <div className={styles.loader__container}>
-            <img src={loading} alt="Loading" className={styles.loader__svg}/>
-        </div>
-    )
+export default function Loading() {
+	return (
+		<div className={styles.container}>
+			<img src={loading} alt="Loading" />
+		</div>
+	)
 }
