c167a464cde1989b0cde10ce04bc06434850ed97
Jobe Moraes <jobecamera@gmail.com>
2023-08-12 21:14:05 -0300

    alterando caminho dos arquivos de WorldList

--- src/pages/Worlds/index.jsx
+++ src/pages/Worlds/index.jsx
@@ -1,47 +1,46 @@
-// import Form from 'components/Form';
 import Loading from 'components/Loading';
 import Records from 'components/Records';
-import WorldsList from 'components/WorldsList';
+import WorldsList from 'components/Worlds/WorldsList';
 import React, { useEffect, useState } from 'react';
 
 export default function Worlds() {
 
-    const [records, setRecords] = useState([]);
-    const [worlds, setWorlds] = useState([]);
-    const [removeLoading, setRemoveLoading] = useState(false)
+	const [records, setRecords] = useState([]);
+	const [worlds, setWorlds] = useState([]);
+	const [removeLoading, setRemoveLoading] = useState(false)
 
-    // Records
-    useEffect(() => {
-        const records = async () => {
-            const response = await fetch('https://api.tibiadata.com/v3/worlds');
-            const jsonData = await response.json();
-            setRecords(jsonData.worlds);
-            setRemoveLoading(true)
-        };
+	// Records
+	useEffect(() => {
+		const records = async () => {
+			const response = await fetch('https://api.tibiadata.com/v3/worlds');
+			const jsonData = await response.json();
+			setRecords(jsonData.worlds);
+			setRemoveLoading(true)
+		};
 
-        records();
+		records();
 
-    }, [])
+	}, [])
 
-    // Worlds
-    useEffect(() => {
-        const worlds = async () => {
-            const response = await fetch('https://api.tibiadata.com/v3/worlds');
-            const jsonData = await response.json();
-            setWorlds(jsonData.worlds.regular_worlds);
-            setRemoveLoading(true)
-        };
+	// Worlds
+	useEffect(() => {
+		const worlds = async () => {
+			const response = await fetch('https://api.tibiadata.com/v3/worlds');
+			const jsonData = await response.json();
+			setWorlds(jsonData.worlds.regular_worlds);
+			setRemoveLoading(true)
+		};
 
-        worlds();
+		worlds();
 
-    }, []);
+	}, []);
 
-    return (
-        <>
-            <Records records={records}>
-                {!removeLoading && <Loading />}
-            </Records>
-            <WorldsList worlds={worlds}/>
-        </>
-    )
+	return (
+		<>
+			<Records records={records}>
+				{!removeLoading && <Loading />}
+			</Records>
+			<WorldsList worlds={worlds} />
+		</>
+	)
 }
