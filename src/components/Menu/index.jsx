3ee0370c2f50b4a777bbd3afe3a2120da8ee6fa5
Jobe Moraes <jobecamera@gmail.com>
2023-11-05 16:28:02 -0300

    ajustes de layout no Menu

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import styles from './Menu.module.scss'
-import MenuLink from 'components/MenuLink'
-
+import React from 'react';
+import styles from './Menu.module.scss';
+import MenuLink from 'components/MenuLink';
+import Logo from '../../../src/assets/svg/favicon.ico';
 
 export default function Menu() {
   return (
     <header>
-      <nav className={styles.nav}>
-        <ul>
-          <MenuLink to="/">
-            Home
-          </MenuLink>
-          <MenuLink to="creatures">
-            Creatures
-          </MenuLink>
-          <MenuLink to="boostablebosses">
-            Boostable Bosses
-          </MenuLink>
-          <MenuLink to="characters">
-            Characters
-          </MenuLink>
-          <MenuLink to="worlds">
-            Worlds
-          </MenuLink>
-        </ul>
-      </nav>
+      <section className={styles.container}>
+        <div className={styles.logoContainer}>
+          <img src={Logo} alt='Logo' />
+          <h1>Tibia Data</h1>
+        </div>
+        <nav>
+          <ul>
+            <MenuLink to="/">
+              Home
+            </MenuLink>
+            <MenuLink to="creatures">
+              Creatures
+            </MenuLink>
+            <MenuLink to="boostablebosses">
+              Boostable Bosses
+            </MenuLink>
+            <MenuLink to="characters">
+              Characters
+            </MenuLink>
+            <MenuLink to="worlds">
+              Worlds
+            </MenuLink>
+          </ul>
+        </nav>
+      </section>
     </header>
-  )
+  );
 }
