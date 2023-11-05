bb0caed6ce05849860510bf713d924e6c9b90e7e
Jobe Moraes <jobecamera@gmail.com>
2023-11-05 17:47:54 -0300

    ajustando responsividade do Menu

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Menu.module.scss';
 import MenuLink from 'components/MenuLink';
 import Logo from '../../../src/assets/svg/favicon.ico';
 
 export default function Menu() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const backgroundMenuHamburguer = 'https://static.tibia.com/images/global/header/mobile/button-icon-menu.png'
+
+
   return (
     <header>
       <section className={styles.container}>
@@ -11,6 +20,9 @@ export default function Menu() {
           <img src={Logo} alt='Logo' />
           <h1>Tibia Data</h1>
         </div>
+        <button onClick={toggleMenu}>
+          <img src={backgroundMenuHamburguer} alt='Menu hamburguer' />
+        </button>
         <nav>
           <ul>
             <MenuLink to="/">
