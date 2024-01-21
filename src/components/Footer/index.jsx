648feb648db59485120338e59a1a3764c9c8f566
Jobe Moraes <jobecamera@gmail.com>
2024-01-21 18:20:46 -0300

    feat: responsividade do footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,16 +3,16 @@ import styles from './Footer.module.scss';
 import { openNewTab } from 'components/SharedFunctions';
 
 export default function Footer() {
-    return (
-        <footer className={styles}>
-            <section>
-                <p>Created by <span className={styles.footer__link} onClick={openNewTab('https://github.com/jobemcamera')}>Jobe Camera</span></p>
-                <p>Tibia is copyrighted by
-                    CipSoft GmbH.
-                    The official website for Tibia is 
-                    <span className={styles.footer__link} onClick={openNewTab('https://www.tibia.com/mmorpg/free-multiplayer-online-role-playing-game.php')}> Tibia.com</span>
-                </p>
-            </section>
-        </footer>
-    )
+	return (
+		<footer className={styles}>
+			<section>
+				<p>Created by <span className={styles.footer__link} onClick={openNewTab('https://github.com/jobemcamera')}>Jobe Camera</span></p>
+				<p>Tibia is copyrighted by
+					CipSoft GmbH.
+					The official website for Tibia is
+					<span className={styles.footer__link} onClick={openNewTab('https://www.tibia.com/mmorpg/free-multiplayer-online-role-playing-game.php')}> Tibia.com</span>
+				</p>
+			</section>
+		</footer>
+	)
 }
