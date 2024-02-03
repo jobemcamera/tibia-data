d2c3c4bc47eb63dc17975d76dd6183a4de5bcc84
Jobe Moraes <jobecamera@gmail.com>
2024-02-03 17:53:31 -0300

    feat: hook useWorlds para fetch da listagem e records de worlds

---
+++ src/hooks/useWorlds.js
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const API_URL = 'https://api.tibiadata.com/v3/worlds';
+
+const fetchData = async () => {
+  const response = await axios.get(API_URL);
+  const result = response?.data?.worlds;
+
+  return result;
+}
+
+export function useWorlds() {
+  const query = useQuery({
+    queryFn: fetchData,
+    queryKey: ['worlds']
+  })
+
+  const worlds = query.data;
+
+  return { ...query, data: worlds };
+}
