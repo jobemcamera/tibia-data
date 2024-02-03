130a01cc6384c4a36ba8ece17804bf6f883a42be
Jobe Moraes <jobecamera@gmail.com>
2024-02-03 17:47:54 -0300

    build: QueryClientProvider na aplicacao

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRoutes from './routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const client = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <AppRoutes />
-    </React.StrictMode>
+	<React.StrictMode>
+		<QueryClientProvider client={client}>
+			<AppRoutes />
+		</QueryClientProvider>
+	</React.StrictMode>
 );
